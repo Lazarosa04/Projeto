ee01e620c722d0969b193cd004e924b40c7b0f65
DSoares16 <129697422+DSoares16@users.noreply.github.com>
2025-05-31 19:17:47 +0100

    Update Viatura.Designer.cs

--- Projeto/Viatura.Designer.cs
+++ Projeto/Viatura.Designer.cs
@@ -36,16 +36,20 @@ namespace Projeto
             this.TBV2 = new System.Windows.Forms.TextBox();
             this.BVEdit = new System.Windows.Forms.Button();
             this.BVRem = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BVAdd
             // 
-            this.BVAdd.Location = new System.Drawing.Point(418, 365);
+            this.BVAdd.Location = new System.Drawing.Point(408, 409);
             this.BVAdd.Name = "BVAdd";
-            this.BVAdd.Size = new System.Drawing.Size(75, 23);
+            this.BVAdd.Size = new System.Drawing.Size(87, 23);
             this.BVAdd.TabIndex = 0;
             this.BVAdd.Text = "Adicionar";
             this.BVAdd.UseVisualStyleBackColor = true;
+            this.BVAdd.Click += new System.EventHandler(this.BVAdd_Click);
             // 
             // comboBox1
             // 
@@ -53,14 +57,14 @@ namespace Projeto
             this.comboBox1.Items.AddRange(new object[] {
             "Ligeiro",
             "Pesado"});
-            this.comboBox1.Location = new System.Drawing.Point(407, 231);
+            this.comboBox1.Location = new System.Drawing.Point(407, 249);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 1;
             // 
             // BVReturn
             // 
-            this.BVReturn.Location = new System.Drawing.Point(713, 415);
+            this.BVReturn.Location = new System.Drawing.Point(713, 409);
             this.BVReturn.Name = "BVReturn";
             this.BVReturn.Size = new System.Drawing.Size(75, 23);
             this.BVReturn.TabIndex = 2;
@@ -79,21 +83,21 @@ namespace Projeto
             // 
             // TBV1
             // 
-            this.TBV1.Location = new System.Drawing.Point(407, 52);
+            this.TBV1.Location = new System.Drawing.Point(407, 78);
             this.TBV1.Name = "TBV1";
             this.TBV1.Size = new System.Drawing.Size(332, 22);
             this.TBV1.TabIndex = 4;
             // 
             // TBV2
             // 
-            this.TBV2.Location = new System.Drawing.Point(407, 139);
+            this.TBV2.Location = new System.Drawing.Point(407, 168);
             this.TBV2.Name = "TBV2";
             this.TBV2.Size = new System.Drawing.Size(332, 22);
             this.TBV2.TabIndex = 5;
             // 
             // BVEdit
             // 
-            this.BVEdit.Location = new System.Drawing.Point(511, 365);
+            this.BVEdit.Location = new System.Drawing.Point(512, 409);
             this.BVEdit.Name = "BVEdit";
             this.BVEdit.Size = new System.Drawing.Size(75, 23);
             this.BVEdit.TabIndex = 6;
@@ -102,18 +106,48 @@ namespace Projeto
             // 
             // BVRem
             // 
-            this.BVRem.Location = new System.Drawing.Point(609, 365);
+            this.BVRem.Location = new System.Drawing.Point(610, 409);
             this.BVRem.Name = "BVRem";
             this.BVRem.Size = new System.Drawing.Size(75, 23);
             this.BVRem.TabIndex = 7;
             this.BVRem.Text = "Remover";
             this.BVRem.UseVisualStyleBackColor = true;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(404, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 16);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Tipo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Ano";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Matrícula";
+            // 
             // Viatura
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.BVRem);
             this.Controls.Add(this.BVEdit);
             this.Controls.Add(this.TBV2);
@@ -140,5 +174,8 @@ namespace Projeto
         private System.Windows.Forms.TextBox TBV2;
         private System.Windows.Forms.Button BVEdit;
         private System.Windows.Forms.Button BVRem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
