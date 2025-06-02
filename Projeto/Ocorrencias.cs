cc6aaabdaaabfd97b5b345a31516f02c3eb7dda2
Lazarosa04 <lazarosa@ua.pt>
2025-06-02 17:21:59 +0100

    load das ocorrências

--- Projeto/Ocorrencias.cs
+++ Projeto/Ocorrencias.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,76 @@ namespace Projeto
 {
     public partial class Ocorrencias : Form
     {
+        // Lista para armazenar as ocorrências
+        private List<OcorrenciaInfo> ocorrencias = new List<OcorrenciaInfo>();
+
+        // Classe interna para representar uma ocorrência
+        private class OcorrenciaInfo
+        {
+            public int Id { get; set; }
+            public string Descricao { get; set; }
+            public string Data { get; set; }
+            public string Local { get; set; }
+
+            public override string ToString() => $"{Descricao} ({Id})";
+        }
+
         public Ocorrencias()
         {
             InitializeComponent();
+            CarregarOcorrencias();
+            this.BOcorrReturn.Click += new EventHandler(BOcorrReturn_Click);
+            this.listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+        }
+
+        private void CarregarOcorrencias()
+        {
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=QuartelBombeiros;Integrated Security=True";
+            string query = "SELECT * FROM Ocorrencia";
+
+            try
+            {
+                listBox1.Items.Clear();
+                ocorrencias.Clear();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var info = new OcorrenciaInfo
+                                {
+                                    Id = Convert.ToInt32(reader["ID_Ocorrencia"]),
+                                    Descricao = reader["Descricao"].ToString(),
+                                    Data = Convert.ToDateTime(reader["Data_Ocorrencia"]).ToString("dd/MM/yyyy"),
+                                    Local = reader["Local"].ToString()
+                                };
+                                ocorrencias.Add(info);
+                                listBox1.Items.Add(info);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar ocorrências: {ex.Message}");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                var selecionada = ocorrencias[listBox1.SelectedIndex];
+                // Exemplo de preenchimento de campos (ajuste conforme seus TextBox)
+                textBoxDescricao.Text = selecionada.Descricao;
+                textBoxData.Text = selecionada.Data;
+                textBoxLocal.Text = selecionada.Local;
+            }
         }
 
         private void BOcorrReturn_Click(object sender, EventArgs e)
