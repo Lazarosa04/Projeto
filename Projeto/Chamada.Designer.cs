091d947359f5297ca4f9e429b81454fbd1f829b4
DSoares16 <129697422+DSoares16@users.noreply.github.com>
2025-06-02 18:34:16 +0100

    Update Chamada.Designer.cs

--- Projeto/Chamada.Designer.cs
+++ Projeto/Chamada.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Projeto
+namespace Projeto
 {
     partial class Chamada
     {
@@ -28,12 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BBRem = new System.Windows.Forms.Button();
+            this.BBEdit = new System.Windows.Forms.Button();
+            this.BBReturn = new System.Windows.Forms.Button();
+            this.BBAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TBV1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(501, 331);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 19);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "Novo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(287, 260);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "Número";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(285, 64);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Nome";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(285, 80);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(250, 20);
+            this.textBox5.TabIndex = 38;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 190);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Data e Hora";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Localização";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Origem";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(287, 205);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 20);
+            this.textBox2.TabIndex = 32;
+            // 
+            // BBRem
+            // 
+            this.BBRem.Location = new System.Drawing.Point(694, 331);
+            this.BBRem.Margin = new System.Windows.Forms.Padding(2);
+            this.BBRem.Name = "BBRem";
+            this.BBRem.Size = new System.Drawing.Size(56, 19);
+            this.BBRem.TabIndex = 31;
+            this.BBRem.Text = "Remover";
+            this.BBRem.UseVisualStyleBackColor = true;
+            // 
+            // BBEdit
+            // 
+            this.BBEdit.Location = new System.Drawing.Point(633, 331);
+            this.BBEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.BBEdit.Name = "BBEdit";
+            this.BBEdit.Size = new System.Drawing.Size(56, 19);
+            this.BBEdit.TabIndex = 30;
+            this.BBEdit.Text = "Editar";
+            this.BBEdit.UseVisualStyleBackColor = true;
+            // 
+            // BBReturn
+            // 
+            this.BBReturn.Location = new System.Drawing.Point(754, 331);
+            this.BBReturn.Margin = new System.Windows.Forms.Padding(2);
+            this.BBReturn.Name = "BBReturn";
+            this.BBReturn.Size = new System.Drawing.Size(56, 19);
+            this.BBReturn.TabIndex = 29;
+            this.BBReturn.Text = "Voltar";
+            this.BBReturn.UseVisualStyleBackColor = true;
+            this.BBReturn.Click += new System.EventHandler(this.BBReturn_Click);
+            // 
+            // BBAdd
+            // 
+            this.BBAdd.Location = new System.Drawing.Point(565, 331);
+            this.BBAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.BBAdd.Name = "BBAdd";
+            this.BBAdd.Size = new System.Drawing.Size(60, 19);
+            this.BBAdd.TabIndex = 28;
+            this.BBAdd.Text = "Adicionar";
+            this.BBAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(287, 139);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 20);
+            this.textBox1.TabIndex = 27;
+            // 
+            // TBV1
+            // 
+            this.TBV1.Location = new System.Drawing.Point(285, 28);
+            this.TBV1.Margin = new System.Windows.Forms.Padding(2);
+            this.TBV1.Name = "TBV1";
+            this.TBV1.Size = new System.Drawing.Size(250, 20);
+            this.TBV1.TabIndex = 26;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(11, 21);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 329);
+            this.listBox1.TabIndex = 25;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(287, 275);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(250, 20);
+            this.textBox3.TabIndex = 43;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(602, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(208, 267);
+            this.richTextBox1.TabIndex = 44;
+            this.richTextBox1.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(599, 12);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "Descrição";
+            // 
+            // Chamada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(840, 369);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.BBRem);
+            this.Controls.Add(this.BBEdit);
+            this.Controls.Add(this.BBReturn);
+            this.Controls.Add(this.BBAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TBV1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Chamada";
             this.Text = "Chamada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BBRem;
+        private System.Windows.Forms.Button BBEdit;
+        private System.Windows.Forms.Button BBReturn;
+        private System.Windows.Forms.Button BBAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TBV1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
