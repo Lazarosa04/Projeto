8236b280e48115607503853abc374f16b12263f4
DSoares16 <129697422+DSoares16@users.noreply.github.com>
2025-06-04 18:49:28 +0100

    Add files via upload

---
+++ Projeto/Chamada.Designer.cs
@@ -0,0 +1,274 @@
+namespace Projeto
+{
+    partial class Chamada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BBRem = new System.Windows.Forms.Button();
+            this.BBEdit = new System.Windows.Forms.Button();
+            this.BBReturn = new System.Windows.Forms.Button();
+            this.BBAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TBV1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1002, 637);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 37);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "Novo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(622, 500);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 25);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "Número";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(618, 123);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 25);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Nome";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(618, 154);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(496, 31);
+            this.textBox5.TabIndex = 38;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(622, 365);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 25);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Data e Hora";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(622, 238);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 25);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Localização";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(618, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Origem";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(622, 394);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(496, 31);
+            this.textBox2.TabIndex = 32;
+            // 
+            // BBRem
+            // 
+            this.BBRem.Location = new System.Drawing.Point(1388, 637);
+            this.BBRem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BBRem.Name = "BBRem";
+            this.BBRem.Size = new System.Drawing.Size(112, 37);
+            this.BBRem.TabIndex = 31;
+            this.BBRem.Text = "Remover";
+            this.BBRem.UseVisualStyleBackColor = true;
+            // 
+            // BBEdit
+            // 
+            this.BBEdit.Location = new System.Drawing.Point(1266, 637);
+            this.BBEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BBEdit.Name = "BBEdit";
+            this.BBEdit.Size = new System.Drawing.Size(112, 37);
+            this.BBEdit.TabIndex = 30;
+            this.BBEdit.Text = "Editar";
+            this.BBEdit.UseVisualStyleBackColor = true;
+            // 
+            // BBReturn
+            // 
+            this.BBReturn.Location = new System.Drawing.Point(1508, 637);
+            this.BBReturn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BBReturn.Name = "BBReturn";
+            this.BBReturn.Size = new System.Drawing.Size(112, 37);
+            this.BBReturn.TabIndex = 29;
+            this.BBReturn.Text = "Voltar";
+            this.BBReturn.UseVisualStyleBackColor = true;
+            this.BBReturn.Click += new System.EventHandler(this.BBReturn_Click);
+            // 
+            // BBAdd
+            // 
+            this.BBAdd.Location = new System.Drawing.Point(1130, 637);
+            this.BBAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BBAdd.Name = "BBAdd";
+            this.BBAdd.Size = new System.Drawing.Size(120, 37);
+            this.BBAdd.TabIndex = 28;
+            this.BBAdd.Text = "Adicionar";
+            this.BBAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(622, 267);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(496, 31);
+            this.textBox1.TabIndex = 27;
+            // 
+            // TBV1
+            // 
+            this.TBV1.Location = new System.Drawing.Point(618, 54);
+            this.TBV1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TBV1.Name = "TBV1";
+            this.TBV1.Size = new System.Drawing.Size(496, 31);
+            this.TBV1.TabIndex = 26;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(22, 40);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(544, 629);
+            this.listBox1.TabIndex = 25;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(622, 529);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(496, 31);
+            this.textBox3.TabIndex = 43;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1204, 54);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(412, 510);
+            this.richTextBox1.TabIndex = 44;
+            this.richTextBox1.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1198, 23);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 25);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "Descrição";
+            // 
+            // Chamada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1680, 710);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.BBRem);
+            this.Controls.Add(this.BBEdit);
+            this.Controls.Add(this.BBReturn);
+            this.Controls.Add(this.BBAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TBV1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Chamada";
+            this.Text = "Chamada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BBRem;
+        private System.Windows.Forms.Button BBEdit;
+        private System.Windows.Forms.Button BBReturn;
+        private System.Windows.Forms.Button BBAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TBV1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Projeto/Chamada.cs
@@ -0,0 +1,204 @@
+// Chamada.cs atualizado para uso de stored procedures
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Projeto
+{
+    public partial class Chamada : Form
+    {
+        private class ChamadaInfo
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public string Descricao { get; set; }
+            public DateTime DataHora { get; set; }
+            public string Numero { get; set; }
+            public string Localizacao { get; set; }
+            public int Origem { get; set; }
+            public string OrigemTexto => Origem == 0 ? "Direta" : "Redirecionada";
+            public override string ToString() => $"({Id}) {Nome}: {DataHora} - ({OrigemTexto})";
+        }
+
+        private List<ChamadaInfo> chamadas = new List<ChamadaInfo>();
+
+        public Chamada()
+        {
+            InitializeComponent();
+            this.Load += Chamada_Load;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            BBAdd.Click += BBAdd_Click;
+            BBRem.Click += BBRemove_Click;
+            button1.Click += BBNovo_Click;
+          
+
+        }
+        private void BBReturn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CarregarChamadas()
+        {
+            string connectionString = "Data Source=PC-DIOGO;Initial Catalog=Teste;Integrated Security=True";
+            chamadas.Clear();
+            listBox1.Items.Clear();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spListarChamadas", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var origemBytes = reader["Origem"] as byte[];
+                                int origem = (origemBytes != null && origemBytes.Length > 0 && origemBytes[0] == 1) ? 1 : 0;
+
+                                var chamada = new ChamadaInfo
+                                {
+                                    Id = Convert.ToInt32(reader["ID_Chamada"]),
+                                    Nome = reader["Nome"].ToString(),
+                                    Descricao = reader["Descrição"].ToString(),
+                                    DataHora = Convert.ToDateTime(reader["Data_Hora_Chamada"]),
+                                    Numero = reader["Número"].ToString(),
+                                    Localizacao = reader["Localização"].ToString(),
+                                    Origem = origem
+                                };
+                                chamadas.Add(chamada);
+                                listBox1.Items.Add(chamada);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar chamadas: {ex.Message}");
+            }
+        }
+
+        private void BBAdd_Click(object sender, EventArgs e)
+        {
+            string nome = textBox5.Text.Trim();
+            string descricao = richTextBox1.Text.Trim();
+            string dataHoraStr = textBox2.Text.Trim();
+            string numero = textBox3.Text.Trim();
+            string localizacao = textBox1.Text.Trim();
+            string origemTexto = TBV1.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(descricao) ||
+                string.IsNullOrWhiteSpace(dataHoraStr) || string.IsNullOrWhiteSpace(numero) ||
+                string.IsNullOrWhiteSpace(localizacao) || string.IsNullOrWhiteSpace(origemTexto))
+            {
+                MessageBox.Show("Preencha todos os campos obrigatórios.");
+                return;
+            }
+
+            if (!DateTime.TryParse(dataHoraStr, out DateTime dataHora))
+            {
+                MessageBox.Show("Data/Hora inválida.");
+                return;
+            }
+
+            int origem = origemTexto.Equals("Redirecionada", StringComparison.OrdinalIgnoreCase) ? 1 : 0;
+            string connectionString = "Data Source=PC-DIOGO;Initial Catalog=Teste;Integrated Security=True";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spAdicionarChamada", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Nome", nome);
+                        command.Parameters.AddWithValue("@Descricao", descricao);
+                        command.Parameters.AddWithValue("@DataHora", dataHora);
+                        command.Parameters.AddWithValue("@Numero", numero);
+                        command.Parameters.AddWithValue("@Localizacao", localizacao);
+                        command.Parameters.AddWithValue("@Origem", origem);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Chamada adicionada com sucesso!");
+                CarregarChamadas();
+                BBNovo_Click(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao adicionar chamada: {ex.Message}");
+            }
+        }
+
+        private void BBRemove_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is ChamadaInfo chamadaSelecionada)
+            {
+                var confirm = MessageBox.Show($"Tem certeza que deseja remover a chamada '{chamadaSelecionada.Nome}'?", "Confirmação", MessageBoxButtons.YesNo);
+                if (confirm == DialogResult.Yes)
+                {
+                    string connectionString = "Data Source=PC-DIOGO;Initial Catalog=Teste;Integrated Security=True";
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            connection.Open();
+                            using (SqlCommand command = new SqlCommand("spRemoverChamada", connection))
+                            {
+                                command.CommandType = CommandType.StoredProcedure;
+                                command.Parameters.AddWithValue("@Id", chamadaSelecionada.Id);
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                        MessageBox.Show("Chamada removida com sucesso!");
+                        CarregarChamadas();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Erro ao remover chamada: {ex.Message}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma chamada para remover.");
+            }
+        }
+
+        private void BBNovo_Click(object sender, EventArgs e)
+        {
+            listBox1.ClearSelected();
+            textBox5.Clear();
+            richTextBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox1.Clear();
+            TBV1.Clear();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is ChamadaInfo chamada)
+            {
+                textBox1.Text = chamada.Localizacao;
+                textBox2.Text = chamada.DataHora.ToString("g");
+                textBox3.Text = chamada.Numero;
+                textBox5.Text = chamada.Nome;
+                TBV1.Text = chamada.OrigemTexto;
+                richTextBox1.Text = chamada.Descricao;
+            }
+        }
+
+        private void Chamada_Load(object sender, EventArgs e)
+        {
+            CarregarChamadas();
+        }
+    }
+}
